68efe6af5bd0bc5eeca0c56cd809b91559fddf4f
murth061 <55423817+murth061@users.noreply.github.com>
2019-10-10 21:36:06 -0500

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+function makePassword(){
+  var length = document.querySelector('#length').value;
+  var max = document.querySelector('#max').value;
+  var min = document.querySelector('#min').value;
+  var easyType = document.getElementById("type").checked;
+  var numberSub = document.getElementById("sub").checked;
+  document.body.style.backgroundColor = blue;
+  }
+
+
+  
